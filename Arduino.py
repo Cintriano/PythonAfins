9918144516795af0f0a7a8bd2d2b9dddda46f7a9
danilo <danisback@rede.ulbra.br>
2025-01-05 19:24:47 -0300

    corrigindo o scrip arduino

--- Arduino.py
+++ Arduino.py
@@ -5,10 +5,9 @@ import tkinter as tk
 def comando_arduino(sinal):
     arduino = serial.Serial('COM9', 9600)
     time.sleep(2)
-    arduino.write(sinal.encode())  # Envia o comando para o Arduino
-    resposta = arduino.readline().decode().strip()  # Lê a resposta do Arduino
-    print(f"Arduino: {resposta}")
+    arduino.write(sinal.encode())  # Envia o comando para o Arduino  # Lê a resposta do Arduino
     arduino.close()
+    print(f"Proceso finalizado")
 
 
 # Criar a janela principal
@@ -17,7 +16,7 @@ janela.title("Comando do Portão")
 janela.geometry("300x100")
 
 # Criar um botão
-botao = tk.Button(janela, text="Executar", command=lambda: comando_arduino(True))
+botao = tk.Button(janela, text="Executar", command=lambda: comando_arduino("True"))
 botao.pack(pady=20)
 
 janela.mainloop()
